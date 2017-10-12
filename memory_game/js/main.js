9521d69cd6a3e4ea984fa7796091ab1429248d23
Paige Boyer <paigeboyer@Paiges-MBP.home>
2017-10-12 13:16:55 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,7 +22,12 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-var checkForMatch = function() {
+
+
+
+var checkForMatch = function(cardId,card) {
+	console.log(this);
+	card.setAttribute('src',cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -33,14 +38,34 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+/* Hint #2: Just as we did in the last step, we can use the `this` keyword to 
+access the element the user just clicked on. Except now instead of getting an 
+attribute, we want to _set_ the `src` attribute.
+
+Hint #3: We'll want to make sure that there are _no quotation marks_ surrounding 
+the new value of the `src` attribute (cards[cardId].cardImage) when we use the 
+`setAttribute()` method.*/
+
+var flipCard = function() {
+	console.log(this);
+	var cardId = this.getAttribute('data-id');
+	console.log(cardId);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	checkForMatch(cardId,this);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for(i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
